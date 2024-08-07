7aa1946872cc9b32e94709dd7a87bf7bd4ad33ea
Anthony Rodriguez <anthony@nezia.dev>
2024-08-07 14:53:27 +0200

    implement connecting with ssh keys

--- pkg/connection/connection.go
+++ pkg/connection/connection.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/charmbracelet/bubbles/list"
 	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 const (
@@ -80,9 +81,9 @@ func (cm *ConnectionManager) AddConnection(host string, user string, port int, p
 	}
 
 	cm.Connections = append(cm.Connections, connection)
-	// TODO: save to disk
 
 	err := cm.SaveToDisk()
+
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -96,13 +97,41 @@ func (cm ConnectionManager) Items() []list.Item {
 	return items
 }
 
+// StartSession starts a new SSH session with the given connection. It will prompt for a password if one was provided, otherwise, it will try to find all available keys in the default paths.
+// TODO: allow custom key paths
 func (c Connection) StartSession() {
+	var authMethods []ssh.AuthMethod
+
+	if c.Password != nil {
+		authMethods = append(authMethods, ssh.Password(*c.Password))
+	} else {
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// assume the keys are located at the default paths
+		keyPaths := []string{
+			fmt.Sprintf("%s/.ssh/id_rsa", homeDir),
+			fmt.Sprintf("%s/.ssh/id_dsa", homeDir),
+			fmt.Sprintf("%s/.ssh/id_ecdsa", homeDir),
+			fmt.Sprintf("%s/.ssh/id_ed25519", homeDir),
+		}
+
+		for _, keyPath := range keyPaths {
+			newAuthMethod, err := publicKeyFile(keyPath)
+			if err != nil {
+				continue
+			}
+
+			authMethods = append(authMethods, newAuthMethod)
+		}
+	}
+
 	config := &ssh.ClientConfig{
-		User: c.Username,
-		Auth: []ssh.AuthMethod{
-			ssh.Password(*c.Password),
-		},
-		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+		User:            c.Username,
+		Auth:            authMethods,
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(), // TODO: add host key verification
 	}
 
 	client, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", c.Host, c.Port), config)
@@ -125,7 +154,8 @@ func (c Connection) StartSession() {
 		ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
 	}
 
-	// Request a pseudo-terminal
+	// TODO: fix escape sequences such as CTRL-L and arrow keys echoing instead of being interpreted
+	// request a pseudo-terminal
 	if err := session.RequestPty("xterm-256color", 80, 40, modes); err != nil {
 		log.Fatal("request for pseudo terminal failed: ", err)
 	}
@@ -134,14 +164,52 @@ func (c Connection) StartSession() {
 	session.Stdin = os.Stdin
 	session.Stderr = os.Stderr
 
-	// Start remote shell
+	// start remote shell
 	if err := session.Shell(); err != nil {
 		log.Fatal(err)
 	}
 
-	// Wait for remote shell to close
+	// wait for remote shell to close
 	err = session.Wait()
 	if err != nil {
 		log.Fatal("Failed to run: " + err.Error())
 	}
+}
+
+func publicKeyFile(file string) (ssh.AuthMethod, error) {
+	key, err := os.ReadFile(file)
+
+	if err != nil {
+		return nil, err
+	}
+
+	signer, err := ssh.ParsePrivateKey(key)
+
+	if err == nil {
+		return ssh.PublicKeys(signer), nil
+	}
+
+	fmt.Printf("Enter passphrase for key %s:", file)
+	passphrase, err := readPassphrase()
+
+	if err != nil {
+		return nil, err
+	}
+
+	signer, err = ssh.ParsePrivateKeyWithPassphrase(key, []byte(passphrase))
+
+	if err != nil {
+		return nil, err
+	}
+
+	return ssh.PublicKeys(signer), nil
+}
+
+func readPassphrase() (string, error) {
+	password, err := terminal.ReadPassword(int(os.Stdin.Fd()))
+	if err != nil {
+		return "", err
+	}
+
+	return string(password), nil
 }
